516a42c484eaee75698069c14fccddbb5058eedb
Raajit Raj <raajit.raj@gmail.com>
2017-04-16 18:44:33 +0530

    Testing Deployment

--- game/game/settings.py
+++ game/game/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'm1*elpeosnm&syr%3#=iht3gd+qwpkxvi(@%*s7lh)pv7u(b6+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.5', 'localhost', 'rraj.localtunnel.me']
 
 
 # Application definition
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
--- game/static/js/game.js
+++ game/static/js/game.js
@@ -1,57 +1,93 @@
+var letter;
+var hint;
+var word;
+
 $(document).ready(function(){
-    word = getWord()["word"].toUpperCase();
-    console.log(word);
-    var word_length = word.length;
-    var blanks = ''
-    for (var i=0; i<word_length; i++){
-        blanks += '_';
-    }
-    $("#game-stage").html("<div style='height:200px; width:200px'></div><h1 id='blanks'>"+blanks+"</h1>");
-    var options = "<h3>";
-    for (var i = 65; i <= 90; i++) {
-    options += "<button class='options'><span id="+String.fromCharCode(i)+">"+String.fromCharCode(i)+" </button>";
-    }
-    options += '</h3>'
-    $("#game-stage").append(options);
-    $("#blanks").css({"font-weight":0, "letter-spacing": "4px"});
-    $("#game-stage").css({"background-color":"grey",});
-
-    $(".options").click(function(){
-        // alert("you clicked "+ $(this).text());
-        var letters = [];
-        // indices.push(1);
-        for(var i=0; i<word.length;i++) {
-                letters.push(word[i]);
-        }
-        console.log(letters)
-        var indices = [];
-        letter_clicked = $(this).text().toString().trim();
-        for(var j=0; j<letters.length; j++){
-            // console.log(letters[j], letter_clicked.trim().length)
-            if (letters[j] === letter_clicked){
-                console.log("why is it not coming");
-                indices.push(j);
-            }
-        }
-        
-        console.log(indices);
-        if (indices.length == 0){
-            $("#"+$(this).text()).css({"color":"red"});
-        }
-        else{
-            $("#"+$(this).text()).css({"color":"green"});            
-        }
-
-    });
+    getNewWord();
 });
 
-// function loadGame(){
-//     var canvas = document.getElementById("canvas");
-//     var ctx = canvas.getContext("2d");
-//     ctx.font = "20pt 'Montserrat', 'Helvetica Neue', Helvetica, Arial, sans-serif";
-//     // ctx.fontFamily =  ", Helvetica Neue, Helvetica, sans-serif";
-//     DATA = getWord()
-//     // alert(DATA)
-    
-//     ctx.fillText(DATA,10,50);
-// }
+function getNewWord(){
+    var resp = getWord()
+    word = resp['word'].toUpperCase();
+    hint = resp['meaning'];
+    console.log(hint);
+    var word_length = resp['length'];
+    var blanks = resp['blanks'];
+    gamestage='<div id="outer"><img id="hangman"><span id="button-holder1" ><h3><button id="hint"><span id="hint-span" class="glyphicon">&#xe101;</span></button></h3> </span><span id="button-holder2" ><h3><button id="reset"><span id="reset-span" class="glyphicon">&#xe030;</span></button></h3> </span>';
+    $("#game-stage").html(gamestage);
+    $('#hangman').fadeOut("slow", function(){
+      $(this).attr('src',IMAGES[0]).bind('onreadystatechange load', function(){
+         if (this.complete) $(this).fadeIn("slow").css({"background-color": "white"});
+      });
+   });
+    blank_space = "<h1 id='blanks'>"+blanks+"</h1>";
+    $("#game-stage").append(blank_space);
+    var options = "<h2>";
+    for (var i = 65; i <= 90; i++) {
+    options += "<button class='options'><span id="+String.fromCharCode(i)+">"+String.fromCharCode(i)+" </button></div>";
+    }
+    options += '</h2>'
+    $("#game-stage").append(options);
+    $("#blanks").css({"font-weight":0, "letter-spacing": "4px"});
+    $("#game-stage").css({"background-color":"black",});
+
+    $(".options").click(function(){
+        letter = $(this).text().trim();
+        $(this).prop('disabled', true);
+        sendLetter();
+    });
+    $("#hint").on('click', function(){
+        if ($('#hint-text').text() === ''){
+        $('#hint-text').animate("slow", function(){
+            $('#hint-text').text(hint);
+        })
+    }
+    else{
+        $('#hint-text').fadeIn("slow").html("<br><br>");
+    }});
+    $("#reset").on('click', function(){
+        console.log();
+        $('#hint-text').fadeIn("slow").html("<br><br>");
+        $("#hangman").attr("src", IMAGES[7])
+        $("#hangman").animate({backgroundColor: "red"}, 'slow')
+        .fadeOut("slow", function(){
+        $("#blanks").text(word);
+            getNewWord();})
+    });
+}
+
+function sendLetter(){
+    $.ajax({
+        url : "/word", // the endpoint
+        type : "GET", // http method
+        dataType : 'json',
+        global: false,
+        data: {"letter":letter},
+        async: false,
+        // handle a successful response
+        success : function(json){
+            console.log(json); // log the returned json to the console
+            console.log(json['blanks']);
+            $("#"+letter).css({"color":json['color']});
+            
+            // $("#"+letter).button('disable');
+            $("#blanks").text(json['blanks']);
+            updateHangman(json['guesses'], json['word'], json['winning']);
+        },
+        error: function(xhr, status, error) {
+        var err = eval("(" + xhr.responseText + ")");
+        alert(err.Message);
+        }})};
+
+function updateHangman(guesses, word, winning){
+    $("#hangman").attr("src", IMAGES[7-guesses]);
+    if (guesses === 0 || winning===true){ 
+        $(".options").prop('disabled', true);
+        $("#blanks").text(word);
+        $("#hangman").attr("src", IMAGES[7-guesses])
+        if (winning===true){$("#hangman").animate({backgroundColor: "green"}, 'slow');}
+        else {$("#hangman").animate({backgroundColor: "red"});}
+        $("#hangman").fadeOut("slow", function(){
+            getNewWord();}
+        )}
+}
